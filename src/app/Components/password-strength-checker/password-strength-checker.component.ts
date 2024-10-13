8ec5b2732d3c56410523cbbb891cd6516445d56c
Ali Hassan <al.m1420@yahoo.com>
2024-10-13 14:00:15 +0300

    use FormBuilder for the form and create function

--- src/app/Components/password-strength-checker/password-strength-checker.component.ts
+++ src/app/Components/password-strength-checker/password-strength-checker.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-password-strength-checker',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordStrengthCheckerComponent implements OnInit {
 
-  constructor() { }
+  strengthForm = this.fb.group({
+    password: ['', [Validators.required, Validators.minLength(4)]]
+  });
+  strength: string = '';
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+  }
+
+  checkStrength(): void {
+    if (this.strengthForm.valid) {
+      const password = this.strengthForm.get('password')?.value || '';
+      const lengthCriteria = password.length >= 8;
+      const hasUppercase = /[A-Z]/.test(password);
+      const hasLowercase = /[a-z]/.test(password);
+      const hasNumber = /[0-9]/.test(password);
+      const hasSpecialChar = /[!@#$%^&*()_+]/.test(password);
+
+      const strengthScore = [lengthCriteria, hasUppercase, hasLowercase, hasNumber, hasSpecialChar].filter(Boolean).length;
+
+      if (strengthScore === 5) {
+        this.strength = 'Strong';
+      } else if (strengthScore >= 3) {
+        this.strength = 'Moderate';
+      } else {
+        this.strength = 'Weak';
+      }
+    }
   }
 
 }
