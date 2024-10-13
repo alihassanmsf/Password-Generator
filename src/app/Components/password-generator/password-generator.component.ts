e95b8b7bef265b9d75d03cda084b0d51ccfe5f9a
Ali Hassan <al.m1420@yahoo.com>
2024-10-13 13:59:03 +0300

    use FormBuilder for the form and create function

--- src/app/Components/password-generator/password-generator.component.ts
+++ src/app/Components/password-generator/password-generator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-password-generator',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordGeneratorComponent implements OnInit {
 
-  constructor() { }
+  passwordForm = this.fb.group({
+    length: [8, [Validators.required, Validators.min(4), Validators.max(32)]]
+  });
+  password: string = '';
+
+  constructor(private fb: FormBuilder) {}
+
 
   ngOnInit(): void {
   }
-
+  generatePassword(): void {
+    if (this.passwordForm.valid) {
+      const length = this.passwordForm.get('length')?.value || 8;
+      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+';
+      this.password = Array.from({ length }, () =>
+        characters.charAt(Math.floor(Math.random() * characters.length))
+      ).join('');
+    }
+  }
 }
