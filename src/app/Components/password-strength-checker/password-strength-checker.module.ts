e244f500a35330a3ed06903cf0f53c340cfdc119
Ali Hassan <al.m1420@yahoo.com>
2024-10-15 18:29:29 +0300

    Import MatInputModule, MatCheckboxModule, MatButtonModule and MatCardModule

--- src/app/Components/password-strength-checker/password-strength-checker.module.ts
+++ src/app/Components/password-strength-checker/password-strength-checker.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PasswordStrengthCheckerComponent } from './password-strength-checker.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 
 
@@ -9,7 +13,11 @@ import { ReactiveFormsModule } from '@angular/forms';
   declarations: [PasswordStrengthCheckerComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatCardModule,
   ]
 })
 export class PasswordStrengthCheckerModule { }
