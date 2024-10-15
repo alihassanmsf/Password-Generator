f1ef22037a692d6a3e6d629badb716149ca85dd1
Ali Hassan <al.m1420@yahoo.com>
2024-10-15 18:17:36 +0300

    Import the active route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PasswordGenerator';
+
+  activeRoute: string = '';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.activeRoute = this.router.url;
+    });
+  }
 }
