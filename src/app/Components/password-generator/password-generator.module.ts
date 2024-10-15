0fcb63fe9a967c7e87ee60e0ab45d9ed644d55d1
Ali Hassan <al.m1420@yahoo.com>
2024-10-15 18:26:51 +0300

    Import MatInputModule, MatCheckboxModule, MatButtonModule and MatCardModule

--- src/app/Components/password-generator/password-generator.module.ts
+++ src/app/Components/password-generator/password-generator.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PasswordGeneratorComponent } from './password-generator.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
-
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [PasswordGeneratorComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatCardModule,
   ]
 })
 export class PasswordGeneratorModule { }
